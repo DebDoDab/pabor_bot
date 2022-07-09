6efe3e0656195d18cd4b36793e05bf48709ec7d1
iliayar <iliayar3@gmail.com>
2022-07-09 20:02:42 +0300

    fix versions

--- flake.nix
+++ flake.nix
@@ -4,10 +4,20 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/17b62c338f2a0862a58bb6951556beecd98ccda9";
-    mach-nix.url = "mach-nix/3.4.0";
+
+    pypi-deps-db = {
+      url = "github:DavHau/pypi-deps-db";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.mach-nix.follows = "mach-nix";
+    };
+    mach-nix = {
+      url = "github:DavHau/mach-nix/3.5.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.pypi-deps-db.follows = "pypi-deps-db";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, mach-nix }:
+  outputs = { self, nixpkgs, flake-utils, mach-nix, ... }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
